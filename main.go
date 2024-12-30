fe99f46d880f20433ece4ec7752a34881b137c31
yuasalily <yuasalily1011@gmail.com>
2024-12-30 14:53:01 +0000

    v1 get追加

--- main.go
+++ main.go
@@ -12,11 +12,20 @@ func main() {
 	// ヘルスチェックエンドポイント
 	e.GET("/health", status)
 
+	// GET
+	e.GET("/v1/**", get)
+
 	// サーバー起動
 	e.Logger.Fatal(e.Start(":8080"))
 }
 
 func status(c echo.Context) error {
+	return c.JSON(http.StatusOK, map[string]string{
+		"status": "OK",
+	})
+}
+
+func get(c echo.Context) error {
 	return c.JSON(http.StatusOK, map[string]string{
 		"status": "OK",
 	})
